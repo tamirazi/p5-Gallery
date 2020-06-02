02ac13d605e24d8ce465204b9fde106e0782845a
tamir azizi <tamir.azizi@gmail.com>
2020-06-03 00:33:51 +0300

    circle with perlin

--- circle_with_noise/sketch.js
+++ circle_with_noise/sketch.js
@@ -2,28 +2,32 @@ let sketch;
 
 function setup() {
   sketch = createCanvas(400 - 45, 550 - 45);
-  sketch.parent("myCanvas");
+  sketch.parent('myCanvas');
   background(backgroundColor);
   stroke(mainColor);
   drawStroke();
 
-  let xoff = 0;
-  let yoff = 0;
-  let scl = 0.01;
   let amp = 200;
-  for (let d = 0; d < 360; d += 1) {
-    let v = p5.Vector.fromAngle(radians(d), 1);
-    let xo = map(sin(xoff), -1, 1, 1, 2);
-    let yo = map(cos(yoff), -1, 1, 1, 2);
-    v.setMag(noise(xo, yo) * amp);
+  let maxNoise = values[3];
+  let margins = values[0];
+  let numOfShapes = values[1];
+  strokeWeight(values[2]);
+  translate(width / 2, height / 2);
+  for (let shape = 0; shape < numOfShapes; shape++) {
+    beginShape();
+    stroke(mainColor);
+    for (let d = 0; d <= TWO_PI; d += 0.01) {
+      let v = p5.Vector.fromAngle(d, 1);
+      let xo = map(sin(d), -1, 1, 0.1, maxNoise);
+      let yo = map(cos(d), -1, 1, 0.1, maxNoise);
+      v.setMag(noise(xo, yo) * amp);
 
-    push();
-    translate(width / 2, height / 2);
-    stroke(0);
-    line(0, 0, v.x, v.y);
-    pop();
-    xoff += scl;
-    yoff += scl;
+      noFill();
+      vertex(v.x, v.y);
+    }
+    endShape();
+    amp -= margins;
+    maxNoise += 0.005;
   }
 }
 
